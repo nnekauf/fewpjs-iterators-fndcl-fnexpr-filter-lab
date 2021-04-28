8a41efb2897739941eab7aa39b235b07e6d509da
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-28 02:50:48 -0400

    commit tree 8dfcf7bcbc547e75009f31a22ac618671ab2cf92

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() == string.toLowerCase())
+    }
+    
+    
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver[0].toLowerCase() == string[0].toLowerCase())
+}
+    
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name.toLowerCase() == string.toLowerCase())
+    }
+    
